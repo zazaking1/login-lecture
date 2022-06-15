9d390adff5758ff7ad546dec23b240ba4ebc2e0e
이진욱 <jinuk-lee@ijin-ug-ui-MacBookPro.local>
2022-06-15 16:34:21 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginbtn = document.querySelector("button");
+
+loginbtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+}
+
+
