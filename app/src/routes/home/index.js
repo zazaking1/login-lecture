8c71fe189c483aab45e4891d625e477b4b897d3b
이진욱 <jinuk-lee@ijin-ug-ui-MacBookPro.local>
2022-06-22 12:13:06 +0900

    회원가입 페이지 구현.

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require('./home.ctrl');
 //rendering
 router.get('/', ctrl.output.home);
 router.get('/login', ctrl.output.login);
+router.get('/register', ctrl.output.register);
 router.post('/login', ctrl.process.login);
 
 
